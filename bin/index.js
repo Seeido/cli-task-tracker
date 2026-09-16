1bc49f6297827d48ff41c08b52dfeaadf522ced8
Seeido <101347929+Seeido@users.noreply.github.com>
2026-09-16 21:36:51 +0300

    style: start formatting with Prettier

--- bin/index.js
+++ bin/index.js
@@ -1,12 +1,13 @@
 #!/usr/bin/env node
-import { argv } from 'node:process';
-import { route } from '../src/commands/router.js'
+import { argv } from "node:process";
+import { route } from "../src/commands/router.js";
 
 const args = argv.slice(2);
 
-if (args.length < 1) { //
-    console.log(`Please provide a command to run`);
-    process.exit(1); // No command provided
+if (args.length < 1) {
+  //
+  console.log(`Please provide a command to run`);
+  process.exit(1); // No command provided
 }
 
-route(args);
+console.log(route(args));
