0a12a28421ec91e993c5ec830b12a32e4278ba8e
Seeido <101347929+Seeido@users.noreply.github.com>
2026-09-18 09:55:23 +0300

    style: better formatting for handling objects

--- src/commands/add.js
+++ src/commands/add.js
@@ -1,20 +1,20 @@
 import { addService } from "../services/task-service.js";
 
 export function addCmd(args) {
-  const desc = args[0];
-  if (args.length != 1 || !desc) {
+  const description = args[0];
+  if (args.length != 1 || !description) {
     throw Error(
       `please provide a description as a string\nSample: clitask add "Conquer the world"`,
     );
   }
 
   const obj = {
-    description: desc,
+    description,
   };
 
   try {
     const newTask = addService(obj);
-    return `Task added succesfully! (ID: ${newTask["id"]})`;
+    return `Task added succesfully! (ID: ${newTask.id})`;
   } catch (error) {
     throw error;
   }
