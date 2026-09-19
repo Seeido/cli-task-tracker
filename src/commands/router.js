44b8824d5eb628827b7aad8fa0fdc720c9a0b012
Seeido <101347929+Seeido@users.noreply.github.com>
2026-09-19 22:19:04 +0300

    feat: implement 'help' command

--- src/commands/router.js
+++ src/commands/router.js
@@ -7,6 +7,7 @@ import { listCmd } from "./list.js";
 import { styleText } from "node:util";
 
 const validCommands = [
+  "help",
   "add",
   "update",
   "delete",
@@ -25,10 +26,34 @@ export function route(args) {
       `'${command}' is not a valid command.\n${styleText("blue", "Run 'clitask help' for more information")}`,
     );
   }
+  if (command === "help") {
+    return helpMsg();
+  }
 
   const argsCalled = args.slice(commandIdx + 1);
 
   return taskCommands[command](argsCalled);
+}
+
+function helpMsg() {
+  return `${styleText("blue", "Usage: clitask <command> [options]")}\n\n${styleText("blue", "Commands:")}\n  ${styleText(
+    "green",
+    styleText("green", "add ") + styleText("cyan", "<description>"),
+  )} - Add a new task\n  ${styleText(
+    "green",
+    styleText("green", "update ") +
+      styleText("magenta", "<id>") +
+      styleText("cyan", " <description>"),
+  )} - Update an existing task's description\n  ${styleText(
+    "green",
+    styleText("green", "delete ") + styleText("magenta", "<id>"),
+  )} - Delete a task\n  ${styleText(
+    "green",
+    styleText("green", "mark-in-progress ") + styleText("magenta", "<id>"),
+  )} - Mark a task as in-progress\n  ${styleText(
+    "green",
+    styleText("green", "mark-done ") + styleText("magenta", "<id>"),
+  )} - Mark a task as done\n  ${styleText("green", "list ")}${styleText("cyan", "[status]")} - List tasks (optional status filter)\n`;
 }
 
 const taskCommands = {
